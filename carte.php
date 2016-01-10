c007e4f816a9c47d2689e76c21181a5ff2cd6860
simplon-juliem <michelet.julie@yahoo.fr>
2016-01-10 23:36:17 +0100

    refonte cv

---
+++ carte.php
@@ -0,0 +1,10 @@
+<?php
+echo "Carte?"."<br><br>";
+
+     <form class=''action='validation.php' method='post'>
+
+         <label>Gazole
+        <input type='text' name='carte'/>
+         <p><input type='submit'></p>
+  </form>";
+    ?>
